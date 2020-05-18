2bdacfcca4a2dfb189074fb9b92156f2b0030bd8
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-18 16:28:36 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,5 @@
 # Write your methods here
-def loop_message_five_times(string)
+=begin def loop_message_five_times(string)
   5.times {puts string}
 end
 
@@ -13,11 +13,13 @@ end
 def output_array(array)
   puts array
  end
+=end
 
 def return_string_array(array)
  count=0 
  while count < array.length do 
    array[count].to_s
+   binding.pry
    count+=1 
  end
 end
