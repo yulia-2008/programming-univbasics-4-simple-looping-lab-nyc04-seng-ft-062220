e8e23f044550b6065301ae782ca43eb75d56d908
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-18 15:52:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,21 @@
 # Write your methods here
+def loop_message_five_times(string)
+  5.times {puts string}
+end
+
+
+def loop_message_n_times(string, integer)
+ integer.times {puts string} 
+end 
+
+
+def output_array(array)
+  array.each do|count|
+   print count
+ end
+end 
+ 
+ 
+def return_string_array 
+  
+end
