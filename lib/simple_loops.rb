d7ab34e6064898c39b49c8edd2db244cf1c6f36c
Yuliya Yurkevich <juliana.ny2008@gmail.com>
2020-05-18 16:39:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,10 +17,12 @@ def output_array(array)
 
 def return_string_array(array)
  count=0 
+ new_array=[]
  while count < array.length do 
-   
+   new_array.push(array[count].to_s)
       count+=1 
  end
+ new_array
 end
 
  
